c05fdf18dadf85aa0714087ffbdb3d135d9ec375
max6555 <khanhtoan.le.6555@gmail.com>
2023-03-09 12:50:22 +0700

    first commit

---
+++ CandidateSystem/Shared/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateSystem
+{
+    public class Candidate
+    {
+        public int CandidateID { get; set; }
+        [Required(ErrorMessage = "The Name Can not be empty")]
+        public string CandidateName { get; set; }
+        public DateTime CandidateDateOfBirth { get; set; }
+        public string CandidateAddress { get; set; }
+        public string CandidateNumber{ get; set; }
+        public string CandidateEmail { get; set; }
+        public string Resource { get; set; }
+        public string JobTitle { get; set; }
+        public string JobPosition { get; set; }
+
+    }
+}
